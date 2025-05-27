c55e475afc7806b51be3bbff534c3ec3f7a96506
chrisedeson <edesonchristopher@gmail.com>
2025-05-27 12:19:10 +0100

    Add error handling middleware and error view for improved error management

--- server.js
+++ server.js
@@ -8,6 +8,7 @@
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require("./utilities/index")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -42,6 +43,31 @@ app.set("layout", "./layouts/layout") // not at views root
  *************************/
 app.use(static)
 
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// Inventory routes
+app.use("/inv", inventoryRoute)
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
+
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
@@ -55,9 +81,3 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
-
-// Index route
-app.get("/", baseController.buildHome)
-
-// Inventory routes
-app.use("/inv", inventoryRoute)
